190736632ed8f3be3dc4bc7656c7e1c3b5245dd3
alexgg <alejandrogata7@gmail.com>
2022-02-18 23:20:48 +0100

    script actualizado

--- Practica2UD3AlejandroGata/tiendapuzzles.sql
+++ Practica2UD3AlejandroGata/tiendapuzzles.sql
@@ -1,54 +1,31 @@
-DROP DATABASE if exists tiendapuzzles;
-CREATE DATABASE if not exists tiendapuzzles;
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
 --
-USE tiendapuzzles;
+-- Servidor: 127.0.0.1:3307
+-- Tiempo de generación: 18-02-2022 a las 23:18:45
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
-create table if not exists editorial (
-ideditorial int auto_increment primary key,
-nombre varchar(50) not null,
-telefono varchar(9));
+-- Base de datos: `tiendapuzzles`
 --
-create table if not exists tienda(
-idtienda int auto_increment primary key,
-nombre varchar(50) not null,
-telefono varchar(9));
+
+DELIMITER $$
 --
-create table if not exists comprador(
-idcomprador int auto_increment primary key,
-nombre varchar(50) not null,
-apellidos varchar(150) not null,
-dni varchar(10) not null UNIQUE);
+-- Funciones
 --
-create table if not exists puzzle(
-idpuzzle int auto_increment primary key,
-titulo varchar(50) not null,
-isbn varchar(40) not null UNIQUE,
-precio float not null,
-ideditorial int,
-idcomprador int,
-idtienda int,
-foreign key(ideditorial) references editorial(ideditorial));
---
-create table if not exists comprador_puzzle(
-idcompradorpuzzle int auto_increment primary key,
-idcomprador int,
-idpuzzle int);
--- 
-create table if not exists puzzle_tienda(
-idpuzzletienda int auto_increment primary key,
-idpuzzle int,
-idtienda int,
-precio int);
--- 
-create table if not exists comprador_tienda(
-idcompradortienda int auto_increment primary key,
-idcomprador int,
-idtienda int);
---
-delimiter ||
-create function existeIsbn(f_isbn varchar(40))
-returns bit
-begin
+CREATE DEFINER=`root`@`localhost` FUNCTION `existeIsbn` (`f_isbn` VARCHAR(40)) RETURNS BIT(1) begin
 	declare i int;
     set i = 0;
     while ( i < (select max(idpuzzle) from puzzles)) do
@@ -57,28 +34,9 @@ begin
     set i = i + 1;
     end while;
     return 0;
-end; ||
-delimiter ;
---
-delimiter ||
-create function existeNombreEditorial(f_name varchar(50))
-returns bit
-begin
-	declare i int;
-    set i = 0;
-    while ( i < (select max(ideditorial) from editoriales)) do
-    if  ((select nombre from editorial where ideditorial = (i + 1)) like f_name) then return 1;
-    end if;
-    set i = i + 1;
-    end while;
-    return 0;
-end; ||
-delimiter ;
---
-delimiter ||
-create function existeNombreComprador(f_name varchar(202))
-returns bit
-begin
+end$$
+
+CREATE DEFINER=`root`@`localhost` FUNCTION `existeNombreComprador` (`f_name` VARCHAR(202)) RETURNS BIT(1) begin
 	declare i int;
     set i = 0;
     while ( i < (select max(idcomprador) from comprador)) do
@@ -87,5 +45,255 @@ begin
     set i = i + 1;
     end while;
     return 0;
-end; ||
-delimiter ;comprador_tienda
+end$$
+
+CREATE DEFINER=`root`@`localhost` FUNCTION `existeNombreEditorial` (`f_name` VARCHAR(50)) RETURNS BIT(1) begin
+	declare i int;
+    set i = 0;
+    while ( i < (select max(ideditorial) from editoriales)) do
+    if  ((select nombre from editorial where ideditorial = (i + 1)) like f_name) then return 1;
+    end if;
+    set i = i + 1;
+    end while;
+    return 0;
+end$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comprador`
+--
+
+CREATE TABLE `comprador` (
+  `idcomprador` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellidos` varchar(150) NOT NULL,
+  `dni` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `comprador`
+--
+
+INSERT INTO `comprador` (`idcomprador`, `nombre`, `apellidos`, `dni`) VALUES
+(1, 'Juan', 'Martinez', 'ASD123');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comprador_puzzle`
+--
+
+CREATE TABLE `comprador_puzzle` (
+  `idcompradorpuzzle` int(11) NOT NULL,
+  `idcomprador` int(11) DEFAULT NULL,
+  `idpuzzle` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comprador_tienda`
+--
+
+CREATE TABLE `comprador_tienda` (
+  `idcompradortienda` int(11) NOT NULL,
+  `idcomprador` int(11) DEFAULT NULL,
+  `idtienda` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `editorial`
+--
+
+CREATE TABLE `editorial` (
+  `ideditorial` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `telefono` varchar(9) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `editorial`
+--
+
+INSERT INTO `editorial` (`ideditorial`, `nombre`, `telefono`) VALUES
+(3, 'Editorial1', '654321654'),
+(4, 'Editorial2', '6516');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `puzzle`
+--
+
+CREATE TABLE `puzzle` (
+  `idpuzzle` int(11) NOT NULL,
+  `titulo` varchar(50) NOT NULL,
+  `isbn` varchar(40) NOT NULL,
+  `ideditorial` int(11) DEFAULT NULL,
+  `idcomprador` int(11) DEFAULT NULL,
+  `idtienda` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `puzzle`
+--
+
+INSERT INTO `puzzle` (`idpuzzle`, `titulo`, `isbn`, `precio`, `ideditorial`, `idcomprador`, `idtienda`) VALUES
+(8, 'titulo1', 'isbn1', 1, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `puzzle_tienda`
+--
+
+CREATE TABLE `puzzle_tienda` (
+  `idpuzzletienda` int(11) NOT NULL,
+  `idpuzzle` int(11) DEFAULT NULL,
+  `idtienda` int(11) DEFAULT NULL,
+  `precio` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `puzzle_tienda`
+--
+
+INSERT INTO `puzzle_tienda` (`idpuzzletienda`, `idpuzzle`, `idtienda`, `precio`) VALUES
+(1, 8, 3, 15),
+(2, 8, 3, 45);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tienda`
+--
+
+CREATE TABLE `tienda` (
+  `idtienda` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `telefono` varchar(9) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tienda`
+--
+
+INSERT INTO `tienda` (`idtienda`, `nombre`, `telefono`) VALUES
+(3, 'Tienda1', '859638596'),
+(4, 'Tienda2', '8594'),
+(5, 'Mercadona', '987654321');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `comprador`
+--
+ALTER TABLE `comprador`
+  ADD PRIMARY KEY (`idcomprador`),
+  ADD UNIQUE KEY `dni` (`dni`);
+
+--
+-- Indices de la tabla `comprador_puzzle`
+--
+ALTER TABLE `comprador_puzzle`
+  ADD PRIMARY KEY (`idcompradorpuzzle`);
+
+--
+-- Indices de la tabla `comprador_tienda`
+--
+ALTER TABLE `comprador_tienda`
+  ADD PRIMARY KEY (`idcompradortienda`);
+
+--
+-- Indices de la tabla `editorial`
+--
+ALTER TABLE `editorial`
+  ADD PRIMARY KEY (`ideditorial`);
+
+--
+-- Indices de la tabla `puzzle`
+--
+ALTER TABLE `puzzle`
+  ADD PRIMARY KEY (`idpuzzle`),
+  ADD UNIQUE KEY `isbn` (`isbn`),
+  ADD KEY `ideditorial` (`ideditorial`);
+
+--
+-- Indices de la tabla `puzzle_tienda`
+--
+ALTER TABLE `puzzle_tienda`
+  ADD PRIMARY KEY (`idpuzzletienda`);
+
+--
+-- Indices de la tabla `tienda`
+--
+ALTER TABLE `tienda`
+  ADD PRIMARY KEY (`idtienda`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `comprador`
+--
+ALTER TABLE `comprador`
+  MODIFY `idcomprador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `comprador_puzzle`
+--
+ALTER TABLE `comprador_puzzle`
+  MODIFY `idcompradorpuzzle` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `comprador_tienda`
+--
+ALTER TABLE `comprador_tienda`
+  MODIFY `idcompradortienda` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `editorial`
+--
+ALTER TABLE `editorial`
+  MODIFY `ideditorial` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `puzzle`
+--
+ALTER TABLE `puzzle`
+  MODIFY `idpuzzle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `puzzle_tienda`
+--
+ALTER TABLE `puzzle_tienda`
+  MODIFY `idpuzzletienda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `tienda`
+--
+ALTER TABLE `tienda`
+  MODIFY `idtienda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `puzzle`
+--
+ALTER TABLE `puzzle`
+  ADD CONSTRAINT `puzzle_ibfk_1` FOREIGN KEY (`ideditorial`) REFERENCES `editorial` (`ideditorial`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
